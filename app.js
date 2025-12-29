614ec687dcce0150962d18d1b0185bfe442d47e0
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-12-28 23:35:57 -0800

    add /create-automation with non-automation-creating interface

--- app.js
+++ app.js
@@ -1,3 +1,93 @@
 import app from "./client.js";
 import { getAutomationCreator, saveState } from "./file.js";
 import blocks from "./blocks.js";
+const warn = async (channel, user, text) => await app.client.chat.postEphemeral({
+	channel,
+	user,
+	blocks: blocks.warn(text),
+	text
+});
+const readableValues = values => Object.fromEntries(Object.values(values).map(value => [Object.entries(value)[0][0], Object.entries(value)[0][1]]));
+
+app.command("/create-automation", async ({ ack, body: { user_id }, respond }) => {
+	await ack();
+	const automationCreator = getAutomationCreator();
+	if (!automationCreator.inProgressAutomations[user_id]) automationCreator.inProgressAutomations[user_id] = {
+		automationName: "",
+		automationShortDescription: "",
+		automationLongDescription: "",
+		automationColor: "",
+		automationConfigurationToken: "",
+		automationRefreshToken: ""
+	};
+	saveState(automationCreator);
+	await respond({
+		text: "Create an automation",
+		blocks: blocks.createAutomationStep1(automationCreator.inProgressAutomations[user_id])
+	});
+});
+
+app.action("create-automation-step1", async ({ ack, body: { user: { id: user }, state: { values } }, respond }) => {
+	await ack();
+	const automationCreator = getAutomationCreator();
+	if (!automationCreator.inProgressAutomations[user]) return await respond("Something went wrong. Try running /create-automation again!");
+	values = readableValues(values);
+	let configurationToken = "ignore-automation-configuration-token" in values ? values["ignore-automation-configuration-token"].value : automationCreator.inProgressAutomations[user].automationConfigurationToken;
+	let refreshToken = "ignore-automation-configuration-refresh-token" in values ? values["ignore-automation-configuration-refresh-token"].value : automationCreator.inProgressAutomations[user].automationRefreshToken;
+
+	automationCreator.inProgressAutomations[user] = {
+		...automationCreator.inProgressAutomations[user],
+		automationConfigurationToken: configurationToken,
+		automationRefreshToken: refreshToken
+	};
+	saveState(automationCreator);
+	await respond({
+		text: "Create an automation",
+		blocks: blocks.createAutomationStep1(automationCreator.inProgressAutomations[user])
+	});
+});
+
+app.action("create-automation-step2", async ({ ack, body: { user: { id: user }, channel: { id: channel }, state: { values } }, respond }) => {
+	await ack();
+	const automationCreator = getAutomationCreator();
+	values = readableValues(values);
+	let name = "ignore-automation-name" in values ? values["ignore-automation-name"].value : automationCreator.inProgressAutomations[user].automationName;
+	let shortDesc = "ignore-automation-short-description" in values ? values["ignore-automation-short-description"].value : automationCreator.inProgressAutomations[user].automationShortDescription;
+	let longDesc = "ignore-automation-long-description" in values ? values["ignore-automation-long-description"].value : automationCreator.inProgressAutomations[user].automationLongDescription;
+	let color = "ignore-automation-color" in values ? values["ignore-automation-color"].value : automationCreator.inProgressAutomations[user].automationColor;
+
+	if (!automationCreator.inProgressAutomations[user]) return await respond("Something went wrong. Try running /create-automation again!");
+	if (!name) return await warn(channel, user, "Enter an automation name!");
+	if (name.length > 35) return await warn(channel, user, "Make sure the name is under 35 characters long!");
+	if (shortDesc && shortDesc.length > 140) return await warn(channel, user, "Make sure the short description is under 140 characters long!");
+	if (!longDesc) return await warn(channel, user, "Enter a long description!");
+	if (longDesc.length < 175) return await warn(channel, user, "Make sure the long description is at least 175 characters long!");
+	if (longDesc.length > 4000) return await warn(channel, user, "Make sure the long description is less than 4000 characters long!");
+	if (color) {
+		if (!parseInt(color, 16)) return await warn(channel, user, "Really? That's not a color.");
+		if (parseInt(color, 16) < 0) return await warn(channel, user, "Really? That's not a color.");
+		if (parseInt(color, 16) > 16777215) return await warn(channel, user, "Really? That's not a color.");
+	}
+
+	automationCreator.inProgressAutomations[user] = {
+		...automationCreator.inProgressAutomations[user],
+		automationName: name,
+		automationShortDescription: shortDesc || "",
+		automationLongDescription: longDesc,
+		automationColor: color || "676767"
+	};
+	if (!automationCreator.inProgressAutomations[user].automationConfigurationToken) automationCreator.inProgressAutomations[user].automationConfigurationToken = "";
+	if (!automationCreator.inProgressAutomations[user].automationRefreshToken) automationCreator.inProgressAutomations[user].automationRefreshToken = "";
+	saveState(automationCreator);
+
+	await respond({
+		text: "Enter your configuration tokens!",
+		blocks: blocks.createAutomationStep2(automationCreator.inProgressAutomations[user])
+	});
+});
+
+app.action("create-automation", async ({ ack }) => await ack());
+
+app.action(/^ignore-.+$/, async ({ ack }) => await ack());
+
+app.action("cancel", async ({ ack, respond }) => [await ack(), await respond({ delete_original: true })]);
--- blocks.js
+++ blocks.js
@@ -1,3 +1,230 @@
 const blocks = {};
 
+blocks.warn = text => [
+	{
+		type: "section",
+		text: {
+			type: "mrkdwn",
+			text
+		},
+		accessory: {
+			type: "button",
+			text: {
+				type: "plain_text",
+				text: "Close"
+			},
+			action_id: "cancel"
+		}
+	}
+];
+
+blocks.createAutomationStep1 = ({ automationName, automationShortDescription, automationLongDescription, automationColor }) => [
+	{
+		type: "section",
+		text: {
+			type: "mrkdwn",
+			text: "First, define the basics of your automation. You'll be able to change this later!"
+		}
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			action_id: "ignore-automation-name",
+			placeholder: {
+				type: "plain_text",
+				text: "Max 35 characters"
+			},
+			initial_value: automationName
+		},
+		label: {
+			type: "plain_text",
+			text: "Automation name",
+			emoji: true
+		},
+		optional: false
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			action_id: "ignore-automation-short-description",
+			placeholder: {
+				type: "plain_text",
+				text: "Max 140 characters (optional)"
+			},
+			initial_value: automationShortDescription
+		},
+		label: {
+			type: "plain_text",
+			text: "Short description",
+			emoji: true
+		},
+		optional: true
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			multiline: true,
+			action_id: "ignore-automation-long-description",
+			placeholder: {
+				type: "plain_text",
+				text: "Min 175 characters; max 4000 characters"
+			},
+			initial_value: automationLongDescription
+		},
+		label: {
+			type: "plain_text",
+			text: "Long description",
+			emoji: true
+		},
+		optional: false
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			action_id: "ignore-automation-color",
+			placeholder: {
+				type: "plain_text",
+				text: "Hex code (optional)"
+			},
+			initial_value: automationColor
+		},
+		label: {
+			type: "plain_text",
+			text: "App Color",
+			emoji: true
+		},
+		optional: false
+	},
+	{
+		type: "actions",
+		elements: [
+			{
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":x: Cancel",
+					emoji: true
+				},
+				value: "cancel",
+				action_id: "cancel"
+			},
+			{
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":arrow_forward: Save & Next",
+					emoji: true
+				},
+				value: "create-automation-step2",
+				action_id: "create-automation-step2"
+			}
+		]
+	}
+];
+
+blocks.createAutomationStep2 = ({ automationConfigurationToken, automationRefreshToken }) => [
+	{
+		type: "section",
+		text: {
+			type: "mrkdwn",
+			text: "Enter your app configuration token and your refresh token. The first is used to create and manage your automation, but it expires soon after being issued. The refresh token is used to renew the first token when it expires."
+		}
+	},
+	{
+		type: "section",
+		text: {
+			type: "mrkdwn",
+			text: "To get these tokens, first go to https://api.slack.com/apps. At the bottom, in \"Your App Configuration Tokens,\" click \"Generate Token.\" Make sure to choose this workspace."
+		}
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			action_id: "ignore-automation-configuration-token",
+			placeholder: {
+				type: "plain_text",
+				text: "xoxe.xoxp-TOKEN"
+			},
+			initial_value: automationConfigurationToken
+		},
+		label: {
+			type: "plain_text",
+			text: "Access Token (the first one)",
+			emoji: true
+		},
+		optional: false
+	},
+	{
+		type: "input",
+		element: {
+			type: "plain_text_input",
+			action_id: "ignore-automation-configuration-refresh-token",
+			placeholder: {
+				type: "plain_text",
+				text: "xoxe-TOKEN"
+			},
+			initial_value: automationRefreshToken
+		},
+		label: {
+			type: "plain_text",
+			text: "Refresh Token (the second one)",
+			emoji: true
+		},
+		optional: false
+	},
+	{
+		type: "actions",
+		elements: [
+			{
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":arrow_backward: Save & Back",
+					emoji: true
+				},
+				value: "create-automation-step1",
+				action_id: "create-automation-step1"
+			},
+			{
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":x: Cancel",
+					emoji: true
+				},
+				value: "cancel",
+				action_id: "cancel"
+			},
+			{
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":white_check_mark: Create!",
+					emoji: true
+				},
+				value: "create-automation",
+				action_id: "create-automation"
+			}
+		]
+	},
+	{
+		type: "context",
+		elements: [
+			{
+				type: "plain_text",
+				text: "Your tokens will be used to create and edit your automations, and will remain in the database thereafter. If you want the tokens to be removed, you can do that at any time through /manage-config-tokens."
+			},
+			{
+				type: "mrkdwn",
+				text: "Keep in mind that your automation and tokens are under your account, and that means you can always take control of them through the Slack page at https://api.slack.com/apps if you have issues. The developer of Automation Creator (<https://github.com/lraj23|lraj23> created this open source at <https://github.com/lraj23/automation-creator|lraj23/automation-creator>) is not responsible for the actions taken by your automations."
+			}
+		]
+	}
+];
+
 export default blocks;
